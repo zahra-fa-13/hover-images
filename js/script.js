a6dfb1e9eae9db8ecc01ad8a3ca3f1c5cb842c62
Zahra <166248394+zahra-fa-13@users.noreply.github.com>
2025-02-11 16:38:15 +0330

    final

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
     $("#img1").mouseenter(function(){
-        $("#img1").attr("src", "axs rangi")
+        $("#img1").attr("src", "images/48.png")
     });
     $("#img1").mouseleave(function(){
-        $("#img1").attr("src", "axs siasefid")
+        $("#img1").attr("src", "images/48-2.png")
+    });
+
+    $("#img2").mouseenter(function(){
+        $("#img2").attr("src", "images/49.png")
+    });
+    $("#img2").mouseleave(function(){
+        $("#img2").attr("src", "images/49-2.png")
+    });
+
+    $("#img3").mouseenter(function(){
+        $("#img3").attr("src", "images/49.png")
+    });
+    $("#img3").mouseleave(function(){
+        $("#img3").attr("src", "images/49-2.png")
+    });
+
+    $("#img4").mouseenter(function(){
+        $("#img4").attr("src", "images/48.png")
+    });
+    $("#img4").mouseleave(function(){
+        $("#img4").attr("src", "images/48-2.png")
     });
 });
