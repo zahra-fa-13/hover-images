85d61c41d9c1a9a5b93d95c76062dace278a3a50
Zahra <166248394+zahra-fa-13@users.noreply.github.com>
2025-02-11 10:57:32 +0330

    hover dark img

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#img1").mouseenter(function(){
+        $("#img1").attr("src", "axs rangi")
+    });
+    $("#img1").mouseleave(function(){
+        $("#img1").attr("src", "axs siasefid")
+    });
+});
